5dc07dc6350f4b6043e4e54085df0fae38c74b3c
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2022-06-18 13:18:43 +0300

    Добавьте файлы проекта.

---
+++ CS10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CS10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startСycle = 5;
+            int endCycle = 103;
+            int numberAdding = 7;
+
+            for (int i = startСycle; i < endCycle; i += numberAdding)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
